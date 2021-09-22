47350446f39e769fbc2bb25a410218c464d514d4
TimothyHyland <moldymilk@hotmail.com>
2021-09-22 08:59:49 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a simple discord bot to make a copy of every @tag for a certin user set in config.json into another channel. 
+You will need to run npm install to install Discord.js
+
+You will need to get a development token from discord and the guild and respective channel to log tags into.
+
---
+++ eattheblocks.js
@@ -0,0 +1,64 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const {
+  prefix,
+  token,
+  users
+} = require('./config.json')
+const guild = client.guilds.cache.get('443194073176604684');
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+client.once('ready', () => {
+  console.log('Listening...');
+
+});
+client.login(token);
+
+
+client.on('message', async message => {
+  if (message.channel.type === "dm") return;    //check to make sure its not a Direct Message
+  if (message.author.id === client.user.id) {    //make sure this message is not from bot
+    return;
+  }
+  let adminRole = message.guild.roles.cache.find(guild => guild.name === "Administrator");   //change to eattheblocks team
+  let modRole = message.guild.roles.cache.find(guild => guild.name === "Moderator");      //change to instructors
+
+    // To ignore all bots (including your own):
+    if (message.author.bot) {
+    return;
+}
+
+const args = message.content.slice(prefix.length).trim().split(/ +/);
+const command = args.shift().toLowerCase();
+if (message.mentions.members.size > 0) {
+
+  let ping = message.mentions.users.first().id
+  if (ping === users.Batman) {
+    
+    
+    
+    
+   const channelz = client.channels.cache.get('443198764694372352')
+   let currentDate = new Date();
+   channelz.send(`The message : "${message.content}" by ${message.author} at ${message.createdAt}. Their ID is ${message.author.id}`);
+  }
+ };
+  
+   
+}
+
+ 
+
+)
+
+
+  
+
+
+
+ 
